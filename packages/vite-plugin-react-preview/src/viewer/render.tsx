e92b2ade093758184d667908af29a1d90dea2c1d
Diego Fernandez <1102266+dfernandez79@users.noreply.github.com>
2024-07-24 16:26:16 -0300

    Move ViewContext to the viewer render

--- packages/vite-plugin-react-preview/src/viewer/render.tsx
+++ packages/vite-plugin-react-preview/src/viewer/render.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { lazy, Suspense } from 'react';
 
 import type { Modules } from './types';
-import { findModule } from './util';
-import { NoModulesMessage, NotFoundMessage, Viewer } from './components';
+import { findModule, getComponent } from './util';
+import { NoModulesMessage, NotFoundMessage } from './components';
+import type { ViewerContextValue } from './ViewerContextValue';
+import { ViewerContext } from './ViewerContext';
 
 export const render = (
   container: Element,
@@ -30,13 +33,23 @@ export const render = (
   } else {
     const [moduleName, loader] = found;
 
+    const context: ViewerContextValue = {
+      modules,
+      moduleName,
+      view,
+    };
+
+    const Component = lazy(async () => ({
+      default:
+        getComponent(await loader(), view) ?? (() => <div>View not found</div>),
+    }));
+
     root.render(
-      <Viewer
-        modules={modules}
-        moduleName={moduleName}
-        loader={loader}
-        view={view}
-      />
+      <ViewerContext.Provider value={context}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Component />
+        </Suspense>
+      </ViewerContext.Provider>
     );
   }
 
--- packages/vite-plugin-react-preview/src/viewer/util/getComponent.ts
+++ packages/vite-plugin-react-preview/src/viewer/util/getComponent.ts
@@ -1,4 +1,4 @@
-import { type ElementType } from 'react';
+import type { ComponentType } from 'react';
 import { isValidElementType } from 'react-is';
 
 import { isObject } from './isObject';
@@ -6,7 +6,7 @@ import { isObject } from './isObject';
 export const getComponent = (
   loadedModule: unknown,
   view?: string
-): ElementType | undefined => {
+): ComponentType | undefined => {
   if (!isObject(loadedModule)) return undefined;
 
   const entries = Object.entries(loadedModule);
@@ -17,5 +17,14 @@ export const getComponent = (
       ? (loadedModule['default'] ?? loadedModule[entries[0]![0]!])
       : loadedModule[view];
 
-  return isValidElementType(component) ? component : undefined;
+  // An ElementType is a ComponentType + IntrinsicElements (div, etc).
+  // Other React APIs like React.lazy only accept ComponentType, which
+  // may be a mistake in React's TypeScript definitions. However, to
+  // comply with the React API contracts, I decided to filter anything
+  // that is not a ComponentType. It will be very rare to return only a
+  // string like "div" and if users need to do that, they have an easy
+  // workaround, which is to use `props => <div {...props}>`.
+  return isValidElementType(component) && typeof component !== 'string'
+    ? (component as ComponentType)
+    : undefined;
 };
